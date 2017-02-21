55f97c3367cdba907eb7db383519d3bbf152015b
Paul WB <higgsbison@gmail.com>
2017-02-20 19:10:26 -0800

    add signup and login links to header

--- client/components/Header.js
+++ client/components/Header.js
@@ -1,16 +1,20 @@
 import React, { Component } from 'react';
 import { graphql } from 'react-apollo';
 import currentUser from '../queries/current_user';
+import { Link } from 'react-router';
 
 class Header extends Component {
   renderButtons() {
     const { loading, user } = this.props.data;
     if(loading) { return <div>loading</div> }
     if(user) {
-      return <div>logout</div>
+      return <div>Logout</div>
     } else {
       return (
-        <div>Not Signed In</div>
+        <div>
+        <li><Link to="signup">Sign Up</Link></li>
+        <li><Link to="login">Login</Link></li>
+        </div>
       );
     }
   }
@@ -19,7 +23,12 @@ class Header extends Component {
     return (
       <nav>
         <div className="nav-wrapper">
-          {this.renderButtons()}
+          <Link to="/" className="brand-logo left">
+            Home
+          </Link>
+          <ul className="right">
+            {this.renderButtons()}
+          </ul>
         </div>
       </nav>
     );
