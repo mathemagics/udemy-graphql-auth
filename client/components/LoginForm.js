86b7fb61611bad358479163257c157a6e04b566d
Paul WB <higgsbison@gmail.com>
2017-02-20 19:29:18 -0800

    add loginform component

---
+++ client/components/LoginForm.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+// import AuthForm from './AuthForm';
+
+class LoginForm extends Component {
+  render() {
+    return (
+      <div>Login</div>
+    );
+  }
+}
+
+export default LoginForm;
