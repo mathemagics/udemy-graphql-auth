3a984c311c8f0f36fd0c68b79a33e8a42db99d16
Paul WB <higgsbison@gmail.com>
2017-02-20 18:58:45 -0800

    add new networkInterface to apollo client

--- client/components/Header.js
+++ client/components/Header.js
@@ -4,6 +4,7 @@ import currentUser from '../queries/current_user';
 
 class Header extends Component {
   render() {
+    console.log(this.props);
     if(this.props.data.loading) { return <div>loading</div> }
     return (
       <div>Header</div>
