4b7f02d02a35eb0a7aedba94d11146a4971f0af3
Paul WB <higgsbison@gmail.com>
2017-02-20 19:38:42 -0800

    add auth form component

--- client/components/LoginForm.js
+++ client/components/LoginForm.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 
-// import AuthForm from './AuthForm';
+import AuthForm from './AuthForm';
 
 class LoginForm extends Component {
   render() {
     return (
-      <div>Login</div>
+      <div>
+        <h3>Login</h3>
+        <AuthForm />
+      </div>
     );
   }
 }
