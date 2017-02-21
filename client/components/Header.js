fe3752e9cea4243f46b23d392c49daf17ff9ccca
Paul WB <higgsbison@gmail.com>
2017-02-20 18:51:17 -0800

    add currentUser query

--- client/components/Header.js
+++ client/components/Header.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
+import { graphql } from 'react-apollo';
+import currentUser from '../queries/current_user';
 
 class Header extends Component {
   render() {
+    if(this.props.data.loading) { return <div>loading</div> }
     return (
       <div>Header</div>
     );
   }
 }
 
-export default Header;
+export default graphql(currentUser)(Header);
