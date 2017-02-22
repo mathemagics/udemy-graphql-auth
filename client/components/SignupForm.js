afb41d3beeb415ce6f6ba698b92d845bda3b781a
Paul WB <higgsbison@gmail.com>
2017-02-21 17:46:27 -0800

    add signup mutation and route

--- client/components/SignupForm.js
+++ client/components/SignupForm.js
@@ -5,13 +5,26 @@ import AuthForm from './AuthForm';
 import signup from '../mutations/signup';
 
 class SignupForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      errors: [],
+    };
+  }
+  onSubmit({email, password}) {
+    this.props.mutate({
+      variables: { email, password },
+    });
+  }
   render() {
     return (
       <div>
           <h3>Sign Up!</h3>
-          <Authform onSubmit={this.onSubmit} errors={this.state.errors} />
+          <AuthForm
+            onSubmit={this.onSubmit.bind(this)}
+            errors={this.state.errors}
+          />
       </div>
-
     );
   }
 }
